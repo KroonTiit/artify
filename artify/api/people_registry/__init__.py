a95a78eba2e423a50486e1b5d8493eb96b5446d2
tiit-win <tiitkroon@gmail.com>
2020-04-19 21:54:58 +0300

    adding skills creation

--- artify/api/people_registry/__init__.py
+++ artify/api/people_registry/__init__.py
@@ -14,10 +14,16 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-def get_db():
+def get_people_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = shelve.open("people.db")
+    return db
+
+def get_skills_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("skills.db")
     return db
 
 @app.teardown_appcontext
@@ -43,7 +49,7 @@ def index():
 
 class PeopleList(Resource):
     def get(self):
-        shelf = get_db()
+        shelf = get_people_db()
         keys = list(shelf.keys())
 
         persons = []
@@ -55,26 +61,22 @@ class PeopleList(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        print(request.get_json)
 
-        
+        parser.add_argument('id', action="store", default=None, type=str, location='json')
         parser.add_argument('name', action="store", default=None, type=str, location='json')
         parser.add_argument('agreed', action="store", default=None, type=bool, location='json')
         parser.add_argument('skills', action="append", default=None, type=str, location='json')
 
-
         # Parse the arguments into an object
         args = parser.parse_args()
-        # print(args)
-        shelf = get_db()
-        shelf[args['name']] = args
+        shelf = get_people_db()
+        shelf[args['id']] = args
 
         return {}, 201
 
-
 class Person(Resource):
     def get(self, name):
-        shelf = get_db()
+        shelf = get_people_db()
 
         # If the key does not exist in the data store, return a 404 error.
         if not (name in shelf):
@@ -82,15 +84,49 @@ class Person(Resource):
 
         return {'message': 'Person found', 'data': shelf[name]}, 200
 
-    def delete(self, name):
-        shelf = get_db()
+    def delete(self, id):
+        shelf = get_people_db()
 
         # If the key does not exist in the data store, return a 404 error.
-        if not (name in shelf):
+        if not (id in shelf):
             return {'message': 'Person not found', 'data': {}}, 404
 
-        del shelf[name]
+        del shelf[id]
         return '', 204
+
+class SkillsList(Resource):
+    def get(self):
+        shelf = get_skills_db()
+        keys = list(shelf.keys())
+
+        skills = []
+
+
+        for key in keys:
+            skills.append(shelf[key])
+
+        return {'message': 'Success', 'data': skills}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('skill', action="store", default=None, type=str, location='json')
+        # Parse the arguments into an object
+        args = parser.parse_args()
+        shelf = get_skills_db()
+        shelf[args['skill']] = args
+
+        return {'message': 'Skill registered', 'data': args}, 201 
+
+class RemoveSkills(Resource):
+    def delete(self, skill):
+        shelf = get_skills_db()
+
+        # If the key does not exist in the data store, return a 404 error.
+        if not (skill in shelf):
+            return {'message': 'Skill not found', 'data': {}}, 404
+
+        del shelf[skill]
+        return '', 204 
 
 @app.after_request
 def after_request(response):
@@ -100,9 +136,10 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
-
-api.add_resource(PeopleList, '/people')
-api.add_resource(Person, '/person/<string:name>')
+api.add_resource(PeopleList, '/people/')
+api.add_resource(Person, '/person/<string:id>')
+api.add_resource(SkillsList, '/skill/')
+api.add_resource(RemoveSkills, '/skills/<string:skill>')
 
 
 
--- artify/src/App.vue
+++ artify/src/App.vue
@@ -69,126 +69,149 @@ export default {
     return {
       selected: null,
       valid: true,
-      name: '',
+      name: localStorage.getItem('name') ? localStorage.getItem('name') : '',
       nameRules: [
         v => !!v || 'Name is required',
         v => (v && v.length >= 2) || 'Name must be more than 2 characters',
       ],
-      select: null,
+      select: localStorage.getItem('skills') ? localStorage.getItem('skills').split(',') : null,
       selectRules: [v => (v && v.length >= 1) || 'How come? You must have some skills!'],
-      checkbox: false,
+      checkbox: localStorage.getItem('agreed') ? localStorage.getItem('agreed') : false,
       checkboxRules: [v => !!v || 'You must be ready to continue!'],
       lazy: false,
-      items: [
-        "Manufacturing",
-        "Construction materials",
-        "Electronics and Optics",
-        "Food and Beverage",
-        "Bakery & confectionery products",
-        "Beverages",
-        "Fish & fish products ",
-        "Meat & meat products",
-        "Milk & dairy products ",
-        "Other",
-        "Sweets & snack food",
-        "Furniture",
-        "Bathroom/sauna ",
-        "Bedroom",
-        "Children’s room ",
-        "Kitchen ",
-        "Living room ",
-        "Office",
-        "Other (Furniture)",
-        "Outdoor ",
-        "Project furniture",
-        "Machinery",
-        "Machinery components",
-        "Machinery equipment/tools",
-        "Manufacture of machinery ",
-        "Maritime",
-        "Aluminium and steel workboats ",
-        "Boat/Yacht building",
-        "Ship repair and conversion",
-        "Metal structures",
-        "Other",
-        "Repair and maintenance service",
-        "Metalworking",
-        "Construction of metal structures",
-        "Houses and buildings",
-        "Metal products",
-        "Metal works",
-        "CNC-machining",
-        "Forgings, Fasteners ",
-        "Gas, Plasma, Laser cutting",
-        "MIG, TIG, Aluminum welding",
-        "Plastic and Rubber",
-        "Packaging",
-        "Plastic goods",
-        "Plastic processing technology",
-        "Blowing",
-        "Moulding",
-        "Plastics welding and processing",
-        "Plastic profiles",
-        "Printing ",
-        "Advertising",
-        "Book/Periodicals printing",
-        "Labelling and packaging printing",
-        "Textile and Clothing",
-        "Clothing",
-        "Textile",
-        "Wood",
-        "Other (Wood)",
-        "Wooden building materials",
-        "Wooden houses",
-        "Other",
-        "Creative industries",
-        "Energy technology",
-        "Environment",
-        "Service",
-        "Business services",
-        "Engineering",
-        "Information Technology and Telecommunications",
-        "Data processing, Web portals, E-marketing",
-        "Programming, Consultancy",
-        "Software, Hardware",
-        "Telecommunications",
-        "Tourism",
-        "Translation services",
-        "Transport and Logistics",
-        "Air",
-        "Rail",
-        "Road",
-        "Water"
-      ]
+      items: []
+      // skills: [
+      //   "Manufacturing",
+      //   "Construction materials",
+      //   "Electronics and Optics",
+      //   "Food and Beverage",
+      //   "Bakery & confectionery products",
+      //   "Beverages",
+      //   "Fish & fish products ",
+      //   "Meat & meat products",
+      //   "Milk & dairy products ",
+      //   "Other",
+      //   "Sweets & snack food",
+      //   "Furniture",
+      //   "Bathroom/sauna ",
+      //   "Bedroom",
+      //   "Children’s room ",
+      //   "Kitchen ",
+      //   "Living room ",
+      //   "Office",
+      //   "Other (Furniture)",
+      //   "Outdoor ",
+      //   "Project furniture",
+      //   "Machinery",
+      //   "Machinery components",
+      //   "Machinery equipment/tools",
+      //   "Manufacture of machinery ",
+      //   "Maritime",
+      //   "Aluminium and steel workboats ",
+      //   "Boat/Yacht building",
+      //   "Ship repair and conversion",
+      //   "Metal structures",
+      //   "Other",
+      //   "Repair and maintenance service",
+      //   "Metalworking",
+      //   "Construction of metal structures",
+      //   "Houses and buildings",
+      //   "Metal products",
+      //   "Metal works",
+      //   "CNC-machining",
+      //   "Forgings, Fasteners ",
+      //   "Gas, Plasma, Laser cutting",
+      //   "MIG, TIG, Aluminum welding",
+      //   "Plastic and Rubber",
+      //   "Packaging",
+      //   "Plastic goods",
+      //   "Plastic processing technology",
+      //   "Blowing",
+      //   "Moulding",
+      //   "Plastics welding and processing",
+      //   "Plastic profiles",
+      //   "Printing ",
+      //   "Advertising",
+      //   "Book/Periodicals printing",
+      //   "Labelling and packaging printing",
+      //   "Textile and Clothing",
+      //   "Clothing",
+      //   "Textile",
+      //   "Wood",
+      //   "Other (Wood)",
+      //   "Wooden building materials",
+      //   "Wooden houses",
+      //   "Other",
+      //   "Creative industries",
+      //   "Energy technology",
+      //   "Environment",
+      //   "Service",
+      //   "Business services",
+      //   "Engineering",
+      //   "Information Technology and Telecommunications",
+      //   "Data processing, Web portals, E-marketing",
+      //   "Programming, Consultancy",
+      //   "Software, Hardware",
+      //   "Telecommunications",
+      //   "Tourism",
+      //   "Translation services",
+      //   "Transport and Logistics",
+      //   "Air",
+      //   "Rail",
+      //   "Road",
+      //   "Water"
+      // ]
     }
   },
   methods: {
     async saveForm() {
-       let data = {
-          name: this.name,
-          agreed: this.checkbox,
-          skills: this.select
-        };
-        await fetch(baceUrl+'/people', {
-          method: 'POST',
-          body:  JSON.stringify(data),
-          headers: new Headers({
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          }),
-          credentials:  'same-origin',
-          mode: 'cors',
-        })
-        .then( (res) => {
-          console.log(res)
-        })
-        .catch( () => {
-          console.log("fail")
-        });
+      localStorage.setItem('name', this.name);
+      localStorage.setItem('agreed', this.checkbox); 
+      localStorage.setItem('skills', this.select);
+      let data = {
+        id: localStorage.getItem('randid'),
+        name: this.name,
+        agreed: this.checkbox,
+        skills: this.select
+      };
+      await fetch(baceUrl + '/people/', {
+        method: 'POST',
+        body:  JSON.stringify(data),
+        headers: new Headers({
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }),
+        credentials:  'same-origin',
+        mode: 'cors',
+      })
+      .then( () => {
+        alert('Form submitted succesfully')
+      })
+      .catch( () => {
+        alert('Form submision failed. Please contact IT services at +372 53 462 198')
+      });
     },
-     validate () {
+    validate () {
       this.$refs.form.validate() && this.saveForm()
     }
+  },
+  async mounted () {
+    const items = await fetch(baceUrl + '/skill/', {
+        method: 'GET',
+        credentials:  'same-origin',
+        mode: 'cors',
+      })
+      .then((res) => {
+       return res.json()
+      })
+      .catch( () => {
+        alert('Fetching skills failed')
+      });
+      const skills = []
+      items.data.forEach(element => {
+        skills.push(element.skill)
+      });
+      this.items = skills
   },
   props: {
     source: String,
